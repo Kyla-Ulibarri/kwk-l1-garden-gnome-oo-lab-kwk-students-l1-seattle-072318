b578836edef4400b97617c0879fc912018d212ed
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-27 23:34:25 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,16 @@
-# Code your instances here
+
+class GardenGnome
+  
+  attr_accessor :name, :age, :gluten_allergy, :hat_color
+  attr_reader :personality
+  
+  def initialize
+    @personality = "evil"
+    @hat_color = "red"
+  end
+  
+  def gnaw
+    puts "Gnawing on a tree!!!"
+  end
+  
+end
